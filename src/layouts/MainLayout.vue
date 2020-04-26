ca00d0db33565093c731c6c55bac48a1a6c2315b
Maria Guarecuco <mgabriela.guarecuco@gmail.com>
2020-04-26 17:14:23 -0400

    First commit (Quasar setup)

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header bordered class="bg-dark text-primary">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg" />
+          </q-avatar>
+          Luminarly
+        </q-toolbar-title>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer bordered class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          Footer
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink';
+
+export default {
+  name: 'MainLayout'
+};
+</script>
